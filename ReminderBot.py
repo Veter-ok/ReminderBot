94d07f24ef1195a32fafd11e45d4f5ee3737e96e
Veter-ok <61391385+Veter-ok@users.noreply.github.com>
2020-10-06 11:13:10 +0300

    Update ReminderBot.py

--- ReminderBot.py
+++ ReminderBot.py
@@ -15,7 +15,7 @@ bot = telebot.TeleBot(str(TOKEN))
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	bot.send_message ( message.chat.id,"Бот запущен" )
+	#bot.send_message ( message.chat.id,"Бот запущен" )
 	while True:
 		olimpiad = cheackData()
 		bot.send_message ( message.chat.id,"р" )
