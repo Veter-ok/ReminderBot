56e6ce8a07ac310985964f59f0e3a928a1526ea6
Veter-ok <61391385+Veter-ok@users.noreply.github.com>
2020-10-12 14:13:09 +0300

    Update ReminderBot.py

--- ReminderBot.py
+++ ReminderBot.py
@@ -1,10 +1,9 @@
 import telebot
 from telebot import types
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
 from time import sleep
 import os
+import ParserLinks
 
 TOKEN = os.environ.get('BOT_TOKEN')
 olimpiades = {"12.10" : "истории", "13.10" : "технологии и итальянскому языку", "14.10" : "литературе",
@@ -33,11 +32,11 @@ def start(message):
 			bot.send_message ( message.chat.id,"Доброе утро, ребята!" )
 			bot.send_message(message.chat.id, "Напоминаю, что сегодня проводится олимпиада по {}. Прошу отписаться тех, кто примет участие".format(cheackData()))
 			morning_message = True
-		elif now.hour == 14 and morning_message:
+		elif now.hour == 14 and olimpiada != None:
 			bot.send_message ( message.chat.id,"Добрый вечер, ребята! У выс есть ещё время, чтобы принять участие в олипиаде по {}".format (cheackData () ) )
 			morning_message = False
 			del olimpiades[olimpiad]
-		if messageLesson != None and not sendLesson and (now.hour == 7 and now.minute <= 15):
+		if messageLesson != None and not sendLesson and (now.hour == 11 and now.minute <= 30):
 			bot.send_message ( message.chat.id, messageLesson)
 			sendLesson = True
 			sendDate = now.day
@@ -55,7 +54,7 @@ def cheackData():
 		
 def createMessage():
 	now = datetime.now()
-	all_parser = parser_links()
+	all_parser = ParserLinks.parser_links()
 	links = all_parser[0]
 	time = all_parser[1]
 	message = "Что у вас сегодня:\n"
@@ -69,67 +68,13 @@ def createMessage():
 		return None
 	else:
 		return str(message)
+
 	
-
-def get_html(url):
-	HEADERS = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
-	html = requests.get(url, headers=HEADERS)
-	return html
-
-def get_content(html):
-	soup = BeautifulSoup(html, 'html.parser')
-	all_events = soup.find_all("td", class_ = "event-eventInfo")
-	all_time_events = soup.find_all("td", class_="event-time")
-	all_links_events = soup.find_all("a", class_="event-link")
-	event_time = []
-	link_events = []
-	for i in range(0, len(all_events)):
-		if all_events[i].text[:3] == "8-1":
-			link_events.append(all_links_events[i].get('href'))
-			event_time.append(all_time_events[i].text)
-	last_links = get_last_content_links(link_events)
-	event_time = event_time[:len(last_links)]
-	return last_links, event_time
-
-
-def parser_links():
-	URL = "https://calendar.google.com/calendar/u/0/htmlembed?height=1200&wkst=1&bgcolor=%23ffffff&ctz=Europe/Moscow&src=c3JqOWdvcHRmdG9rNGc4aWhlZDExYmVoNnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230B8043&showNav=1&showPrint=0&showTabs=0&showCalendars=0&mode=AGENDA&pli=1"
-	html = get_html(URL)
-	if html.status_code == 200:
-		return get_content(html.text)
-	else:
-		return None
-
-def get_last_content_links(links):
-	last_links = []
-	for link in links:
-		html = get_html("https://calendar.google.com/calendar/u/0/{}".format(link))
-		if html.status_code == 200:
-			i = get_last_links(html.text)
-			if i != None:
-				last_links.append(i)
-		else:
-			return None
-	return last_links
-
-
-def get_last_links(html):
-	soup = BeautifulSoup(html, "html.parser")
-	date = str(soup.find_all("time")[1].get("datetime"))[4:8]
-	if int(date[:2]) == 10 and int(date[2:]) <= 18:
-		link = soup.find_all("a")[-1].get('href')
-		title = soup.find("title").text
-		return [date, link, title]
-	else:
-		return None
-
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
 	#bot.send_message(message.chat.id, message.text)
 	pass
-
-
 
 
 bot.polling(none_stop=True)
